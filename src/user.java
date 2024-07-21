e4d7ea9fceac9dd348d3c87836e0edcd76aa4661
liushang123 <634012658@qq.com>
2024-07-21 18:24:33 +0800

    增加一个super方法

--- src/user.java
+++ src/user.java
@@ -2,4 +2,12 @@ public class user {
     int id;
     String name;
 
+    public user() {
+        super();
+    }
+
+    public user(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 }
