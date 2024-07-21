d1e037813352d957b188a1c2c48533db84e178da
liushang123 <634012658@qq.com>
2024-07-21 21:27:30 +0800

    增加tostring方法

--- src/user.java
+++ src/user.java
@@ -6,8 +6,18 @@ public class user {
         super();
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
     public user(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
